4a6374cef2d9db720d77fa7bbe1326de6b074582
Patrick Neubauer <patrick.neubauer@york.ac.uk>
2020-01-03 15:09:35 +0100

    extends evaluation POC with roundtrip use case (serialize model of language 2 using serializer of language 1 and subsequently parse model serialized with language 1 serializer with parser of language 2; finally, assert that there are no differences)

--- evaluation-poc/uk.ac.york.cs.ecss.evaluation/src/main/java/uk/ac/york/cs/ecss/evaluation/EvaluationPoc.java
+++ evaluation-poc/uk.ac.york.cs.ecss.evaluation/src/main/java/uk/ac/york/cs/ecss/evaluation/EvaluationPoc.java
@@ -3,7 +3,12 @@ package uk.ac.york.cs.ecss.evaluation;
 import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
@@ -11,6 +16,8 @@ import org.eclipse.emf.compare.Comparison;
 import org.eclipse.emf.compare.Diff;
 import org.eclipse.emf.compare.DifferenceKind;
 import org.eclipse.emf.compare.EMFCompare;
+import org.eclipse.emf.compare.Equivalence;
+import org.eclipse.emf.compare.Match;
 import org.eclipse.emf.compare.match.DefaultComparisonFactory;
 import org.eclipse.emf.compare.match.DefaultEqualityHelperFactory;
 import org.eclipse.emf.compare.match.DefaultMatchEngine;
@@ -23,8 +30,10 @@ import org.eclipse.emf.compare.scope.IComparisonScope;
 import org.eclipse.emf.compare.utils.UseIdentifiers;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtext.parser.antlr.AbstractAntlrParser;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.resource.XtextResourceSet;
+import org.eclipse.xtext.serializer.impl.Serializer;
 
 import com.google.inject.Injector;
 
@@ -42,28 +51,30 @@ public class EvaluationPoc {
 		
 		// Language 1
 		Injector injector1 = new org.xtext.example.mydsl1.MyDsl1StandaloneSetup().createInjectorAndDoEMFRegistration();
+		Serializer serializer1 = injector1.getInstance(Serializer.class);
 		System.out.println("Finished with Language 1 setup !");
 		XtextResourceSet resourceSet1 = injector1.getInstance(XtextResourceSet.class);
 		resourceSet1.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
-		Resource resource = resourceSet1.getResource(
+		Resource resource1 = resourceSet1.getResource(
 		    URI.createURI("models/model1.mydsl1"), true);
-//		Statemachine model1 = (Statemachine) resource.getContents().get(0);
+		Statemachine model1 = (Statemachine) resource1.getContents().get(0);
 		
 		// Language 2 with equal model
 		Injector injector2 = new org.xtext.example.mydsl2.MyDsl2StandaloneSetup().createInjectorAndDoEMFRegistration();
+		Serializer serializer2 = injector2.getInstance(Serializer.class);
 		System.out.println("Finished with Language 2 setup !");
 		XtextResourceSet resourceSet2 = injector2.getInstance(XtextResourceSet.class);
 		resourceSet2.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
 		Resource resource2 = resourceSet2.getResource(
 		    URI.createURI("models/model1.mydsl2"), true);
-//		Statemachine model2 = (Statemachine) resource2.getContents().get(0);
+		Statemachine model2 = (Statemachine) resource2.getContents().get(0);
 		
 		// Language 2 with differentiating model
 		XtextResourceSet resourceSet3 = injector2.getInstance(XtextResourceSet.class);
 		resourceSet3.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
 		Resource resource3 = resourceSet3.getResource(
 		    URI.createURI("models/model2.mydsl2"), true);
-//		Statemachine model3 = (Statemachine) resource3.getContents().get(0);
+		Statemachine model3 = (Statemachine) resource3.getContents().get(0);
 		
 		// -------
 		
@@ -93,7 +104,39 @@ public class EvaluationPoc {
 		assertTrue(diffList2.size()==1);
 		assertTrue(diffList2.get(0).getKind()==DifferenceKind.DELETE);
 
-		System.out.println("Finished comparison !");
+		
+		// get equivalences
+		EList<Equivalence> eqList1 = comparison1.getEquivalences();
+		EList<Equivalence> eqList2 = comparison2.getEquivalences();
+		
+		// get matches
+		EList<Match> matchList1 = comparison1.getMatches();
+		EList<Match> matchList2 = comparison2.getMatches();
+		
+		// ----------
+		
+		// serialize model of language 2 using serializer of language 1
+		try {
+			Files.write(Paths.get("models/generated/model2.mydsl1"), serializer1.serialize(model3).getBytes(), StandardOpenOption.CREATE);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// parse model serialized with language 1 serializer with parser of language 2
+		XtextResourceSet resourceSet4 = injector2.getInstance(XtextResourceSet.class);
+		resourceSet4.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
+
+		Resource resource4 = resourceSet4.getResource(
+			    URI.createURI("models/generated/model2.mydsl1"), true);
+		Statemachine model4 = (Statemachine) resource4.getContents().get(0);
+
+		IComparisonScope scope3 = EMFCompare.createDefaultScope(resourceSet3, resourceSet4);
+		Comparison comparison3 = comparator.compare(scope3);
+		EList<Diff> diffList3 = comparison3.getDifferences();		
+		assertTrue(diffList3.size()==0);
+		
+		System.out.println("Finished !");
 
 	}
 
